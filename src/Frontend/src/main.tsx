5fca2c3c19b96cb770fd695d332dcf06061b4575
wanony <65881470+wanony@users.noreply.github.com>
2026-06-25 00:15:51 +0100

    feat: wrap app with GlimmProvider (prism palette)

--- src/Frontend/src/main.tsx
+++ src/Frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { GlimmProvider } from 'glimm/react'
 import { AuthProvider } from './auth/AuthProvider'
 import App from './App'
 import './index.css'
@@ -11,11 +12,13 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </QueryClientProvider>
+      <GlimmProvider palette="prism" sweepMs={900} outroMs={600}>
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </QueryClientProvider>
+      </GlimmProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
